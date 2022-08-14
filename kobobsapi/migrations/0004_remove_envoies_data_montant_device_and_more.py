abbde5289df2c30848f9a24cb9ee72a548ea89bd
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-14 12:36:21 +0100

    first commit14

--- kobobsapi/migrations/0004_remove_envoies_data_montant_device_and_more.py
+++ kobobsapi/migrations/0004_remove_envoies_data_montant_device_and_more.py
@@ -10,37 +10,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='envoies_data',
-            name='montant_device',
-        ),
-        migrations.RemoveField(
-            model_name='envoies_data',
-            name='montant_envoie',
-        ),
-        migrations.AddField(
-            model_name='envoies_data',
-            name='adresse_expediteur',
-            field=models.CharField(default='', max_length=100),
-        ),
-        migrations.AddField(
-            model_name='envoies_data',
-            name='frais_tva',
-            field=models.DecimalField(decimal_places=2, default='', max_digits=15),
-        ),
-        migrations.AddField(
-            model_name='envoies_data',
-            name='montant_beneficiaire',
-            field=models.DecimalField(decimal_places=2, default='', max_digits=15),
-        ),
-        migrations.AddField(
-            model_name='envoies_data',
-            name='montant_envoie_sans_frais',
-            field=models.DecimalField(decimal_places=2, default='', max_digits=15),
-        ),
-        migrations.AlterField(
-            model_name='envoies_data',
-            name='status_retrait',
-            field=models.CharField(default='code retrait en attente de validation', max_length=50),
-        ),
+       
     ]

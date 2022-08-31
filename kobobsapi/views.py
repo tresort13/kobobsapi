5adcbce6c1dea20a256949662c01f23fd5178b7d
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-31 00:06:56 -0700

    first commit11

--- kobobsapi/views.py
+++ kobobsapi/views.py
@@ -122,9 +122,8 @@ def envoieFormulaire(request):
         return Response('',status=status.HTTP_400_BAD_REQUEST)
     
     
-class EnvoieFormulaireAbonne(APIView):
-      
-  def post(self, request, *args, **kwargs): 
+@api_view(['POST'])   
+def envoieFormulaireAbonne(request): 
         nom_expediteur= request.data['nom_expediteur']
         postnom_expediteur= request.data['postnom_expediteur']
         prenom_expediteur= request.data['prenom_expediteur']
@@ -139,8 +138,7 @@ class EnvoieFormulaireAbonne(APIView):
         numero_beneficiaire= request.data['numero_beneficiaire']
         pays_beneficiaire= request.data['pays_beneficiaire']
         montant_envoie = request.data['montant_beneficiaire']
-        type_service= request.data['type_service'],
-        code_abonne= request.data['code_abonne']
+        type_service= request.data['type_service']
         
         
         retrait_donnes1 = [23244562,39430944,18034851,34890346,45860984,23409858,23849384,12435646,54677540,65467383]
@@ -175,11 +173,10 @@ class EnvoieFormulaireAbonne(APIView):
         
         montant_total = montant_envoi_convert_sterling + frais_envoie
         
-        
-        serializer = Envoies_dataSerializer(data={'nom_expediteur': nom_expediteur,'postnom_expediteur':postnom_expediteur,'prenom_expediteur' : prenom_expediteur,'adresse_expediteur' : adresse_expediteur,'email_expediteur' : email_expediteur,'numero_expediteur' : numero_expediteur,'pays_expediteur' : pays_expediteur,'nom_beneficiaire' : nom_beneficiaire,'postnom_beneficiaire' : postnom_beneficiaire,'prenom_beneficiaire' : prenom_beneficiaire,'adresse_beneficiaire' : adresse_beneficiaire,'numero_beneficiaire' : numero_beneficiaire,'pays_beneficiaire' : pays_beneficiaire,'montant_envoie_sans_frais':montant_envoi_convert_sterling,'montant_beneficiaire':montant_envoi_convert,'type_service' : type_service, 'frais_envoie' : frais_envoie,'frais_tva':frais_tva,'montant_total': montant_total,'code_retrait':code_retrait,'code_abonne':code_abonne})
+        serializer = Envoies_dataSerializer(data={'nom_expediteur': nom_expediteur,'postnom_expediteur':postnom_expediteur,'prenom_expediteur' : prenom_expediteur,'adresse_expediteur' : adresse_expediteur,'email_expediteur' : email_expediteur,'numero_expediteur' : numero_expediteur,'pays_expediteur' : pays_expediteur,'nom_beneficiaire' : nom_beneficiaire,'postnom_beneficiaire' : postnom_beneficiaire,'prenom_beneficiaire' : prenom_beneficiaire,'adresse_beneficiaire' : adresse_beneficiaire,'numero_beneficiaire' : numero_beneficiaire,'pays_beneficiaire' : pays_beneficiaire,'montant_envoie_sans_frais':montant_envoi_convert_sterling,'montant_beneficiaire':montant_envoi_convert,'type_service' : type_service, 'frais_envoie' : frais_envoie,'frais_tva':frais_tva,'montant_total': montant_total,'code_retrait':code_retrait})
         if serializer.is_valid() :
-           serializer.save()
-           return Response(serializer.data)
+          serializer.save()
+          return Response(serializer.data)
         return Response('',status=status.HTTP_400_BAD_REQUEST)
 
 
